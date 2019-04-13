98f8358b97e491b8f642f8266e643b0944de9e79
jackschu <js9pa@virginia.edu>
2019-04-12 23:23:13 -0400

    starter code for games

--- webmain/games/views.py
+++ webmain/games/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def addGame(request):
+    a = "dataeihfjoqiej"
+    return render(request, 'games/viewGroup.html')
+
+    
+def viewGame(request, game_id=None):
+    return HttpResponse('hi' + str(game_id))
