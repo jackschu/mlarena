ea31b98bfde4690a582d4441e37869c265479a8e
Harun Feraidon <harunferaidon@gmail.com>
2019-04-13 02:40:22 -0400

    fix the file upload

--- webmain/games/models.py
+++ webmain/games/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Game(models.Model):
     name = models.CharField(max_length=50)
     desc = models.TextField(blank=True, null=True)
-    file = forms.FileField()
+    file = models.FileField()
