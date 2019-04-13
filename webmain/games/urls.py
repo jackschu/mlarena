6b8a8d512f35187e399f27baa0fa0cfc565b59ec
jackschu <js9pa@virginia.edu>
2019-04-12 23:31:14 -0400

    commiting basic base template

---
+++ webmain/games/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, re_path
+from django.conf.urls import url
+from django.contrib import admin
+from games import views
+
+app_name = 'games'
+urlpatterns = [
+        path('new/', views.addGame, name='addGame'),
+        path('view/<int:game_id>', views.viewGame, name='viewGame'),
+]
