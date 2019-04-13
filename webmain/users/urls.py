fab7f48656c65cf1f90b4f5d1b0f1c4ea19ae5ca
jackschu <js9pa@virginia.edu>
2019-04-13 00:56:18 -0400

    login working

--- webmain/users/urls.py
+++ webmain/users/urls.py
@@ -2,8 +2,11 @@ from django.urls import path, re_path
 from django.conf.urls import url
 from django.contrib import admin
 from . import views
+from .views import Register
 
 app_name = 'users'
 urlpatterns = [
         path('login/', views.my_login, name='login'),
+    path('register/', Register.as_view(), name='register'),
+    path('logout/', views.my_logout, name ='logout'),    
 ]
--- webmain/users/views.py
+++ webmain/users/views.py
@@ -1,24 +1,38 @@
+from django.views import generic
 from django.contrib import messages
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import login, authenticate, logout
-
-from .forms import LoginForm
+from .models import CustomUser
+from django.urls import reverse, reverse_lazy
+from .forms import LoginForm, CustomUserCreationForm
 # Create your views here.
 def my_login(request):
     if request.method == "POST":
+
         username= request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             messages.success(request, 'Successfully logged in ')
-            return HttpResponseRedirect(reverse('login'))
+            return HttpResponseRedirect(reverse('index'))
         else:
             messages.add_message(request, messages.ERROR, 'No account associated with this email ')
-        return HttpResponse('would redirect to register')
+        return HttpResponse('users:login')
             #          return HttpResponseRedirect(reverse('users:register'))
     else:
         form = LoginForm()
     return render(request, 'users/login.html', {'form' : form})
     
+class Register(generic.CreateView):
+    form_class = CustomUserCreationForm
+    model = CustomUser
+    success_url = reverse_lazy('users:my_login')
+    template_name = 'users/register.html'
+
+
+def my_logout(request):
+    print('hi')
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
