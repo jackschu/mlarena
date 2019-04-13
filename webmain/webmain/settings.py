c5e6a7570a0692559432e4c309a28ac776c2671c
David Thames <davidct@vt.edu>
2019-04-13 00:16:35 -0400

    GCloud Hosting

--- webmain/webmain/settings.py
+++ webmain/webmain/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+^jo=b@89+1&#-z2f@*awrjbi)j5&**au4(*zo*5ot@aiycat#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
