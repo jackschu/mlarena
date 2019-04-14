072e3e6eb3d625c8230e53512981432accb91074
Hamza Mir <hamzasmir@gmail.com>
2019-04-14 05:42:16 -0400

    temporarily use description field as javascript renderer file dump

--- webmain/bots/views.py
+++ webmain/bots/views.py
@@ -38,10 +38,12 @@ def viewMatch(request, match_id=None):
     if match_id:
         match = Match.objects.get(pk=match_id)
         game = match.game
-        game_file = game.renderer_file.path.split('/')[-1]
+        if game.renderer_file and game.renderer_file.path:
+            game_file = game.renderer_file.path.split('/')[-1]
         match_record = MatchRecord.objects.filter(match=match)[0]
         frames = GameFrame.objects.filter(match_record=match_record)
         states = [f.state for f in frames]
         json_data = json.dumps(states)
+        js_text = game.desc.replace('\n', '')
         
-    return render(request, 'bots/canvas.html', {'game_file':game_file, 'states':json_data})
+    return render(request, 'bots/canvas.html', {'game_file':game_file, 'states':json_data, 'js_text': js_text})
